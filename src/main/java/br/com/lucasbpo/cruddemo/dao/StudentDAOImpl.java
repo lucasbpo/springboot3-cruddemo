ffbbcd041662f2dc8c8744902dfb33e492a3df27
lucasbpo <lucasbpo@outlook.com.br>
2025-06-25 09:38:58 -0300

    fix: add `@Transactional` on save method

--- src/main/java/br/com/lucasbpo/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/br/com/lucasbpo/cruddemo/dao/StudentDAOImpl.java
@@ -2,7 +2,10 @@ package br.com.lucasbpo.cruddemo.dao;
 
 import br.com.lucasbpo.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class StudentDAOImpl implements StudentDAO {
 
     private EntityManager entityManager;
@@ -12,6 +15,7 @@ public class StudentDAOImpl implements StudentDAO {
     }
 
     @Override
+    @Transactional
     public void save(Student student) {
         this.entityManager.persist(student);
     }
