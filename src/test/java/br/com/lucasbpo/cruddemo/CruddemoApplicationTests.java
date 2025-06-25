9ddeb7670ec412b0378e6a58b78556d534d09873
lucasbpo <lucasbpo@outlook.com.br>
2025-06-25 08:36:31 -0300

    chore: initial commit.

---
+++ src/test/java/br/com/lucasbpo/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasbpo.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
